2b81a58e1131f57a71a04cfd26a9725979564aeb
haha <haidanglink0@gmail.com>
2025-09-29 09:15:01 +0700

    update

--- src/components/GeneralSubjectComponent/index.tsx
+++ src/components/GeneralSubjectComponent/index.tsx
@@ -2,7 +2,7 @@ import ListSubject from "./ListSubject/ListSubject";
 import type {ListSubjectProps} from './ListSubject/ListSubject'
 
 import AddFormDefault from "./SingleSubject/AddFormDefault/AddFormDefault";
-import EditFormDefault from "./SingleSubject/EditForm/EditFormDefault";
+import EditFormDefault from "./SingleSubject/EditFormDefault/EditFormDefault";
 
 export {ListSubject, AddFormDefault, EditFormDefault}
 export type {
